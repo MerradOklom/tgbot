0cce8a32b597bfd7b036426417486f248e3df398
MerradOklom <43864503+MerradOklom@users.noreply.github.com>
2025-05-03 00:07:31 -0300

    Update dummy-server.js

--- dummy-server.js
+++ dummy-server.js
@@ -2,9 +2,9 @@ import http from 'http';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Dummy HTTP server running on port 8787\n');
+  res.end('Dummy HTTP server running\n');
 });
 
 server.listen(8787, () => {
-  console.log('Dummy HTTP server started on port 8787');
+  console.log('Dummy HTTP server started');
 });
